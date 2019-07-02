8bb9ec457874744837b382ef5fa3aa2cc786d2fb
Lwam Tekie <ltekie87@yahoo.com>
2019-07-02 17:33:44 -0500

    finished Auth

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Home.scss';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <button className="btn btn-danger">Hi</button>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
