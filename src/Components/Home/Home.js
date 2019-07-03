b81fb6677c5f90c69153b6c395670138a919c9ff
Lwam Tekie <ltekie87@yahoo.com>
2019-07-02 20:49:24 -0500

    edit and home work

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+
 import './Home.scss';
 
-function Home() {
-  return (
-    <div className="Home">
-      <header className="Home-header">
-        <button className="btn btn-danger">Hi</button>
-      </header>
-    </div>
-  );
+class Home extends React.Component {
+  editEvent = (e) => {
+    e.preventDefault();
+    const orderId = '12345';
+    this.props.history.push(`/edit/${orderId}`);
+  }
+
+  render() {
+    const singleLink = '/scat/12345';
+    return (
+      <div className="Home">
+        <h1>Home</h1>
+        <button className="btn btn-danger" onClick={this.editEvent}>Edit a thing</button>
+        <Link to={singleLink}>View Single</Link>
+      </div>
+    );
+  }
 }
 
 export default Home;
